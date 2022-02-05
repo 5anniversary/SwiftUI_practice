3918b14aa011ac62d9723a7652085571c1188c17
오준현 <oh.junhyeon95@gmail.com>
2022-02-05 14:56:53 +0900

    :rocket: project setting

---
+++ TooDo/TooDo/TooDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TooDoApp.swift
+//  TooDo
+//
+//  Created by 오준현 on 2022/02/05.
+//
+
+import SwiftUI
+
+@main
+struct TooDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
